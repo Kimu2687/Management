6293a772436cac7a40cdfb5075cc24abeb0ef341
infor@rentoproperties.co.ke <infor@rentoproperties.co.ke>
2021-09-04 11:12:41 -0700

    Restock 5 litres

--- Base_controller/Base_controller.cs
+++ Base_controller/Base_controller.cs
@@ -8,13 +8,24 @@ namespace TASK.Base_controller
 {
     public abstract class BaseController : Controller
     {
+        
         protected virtual void swal(string header, string msg, string msg_type)
         {
             TempData["msg_header"] = header;
             TempData["msg"] = msg;
             TempData["msg_type"] = msg_type;
 
-            // Do stuff
+      
         }
+
+        //GLOBAL AJAX RESPONSE
+        public class Total_cash_made
+        { 
+            public string response_message { get; set; }
+        }
+
+        //FEEDING THE MESSAGE
+        public Total_cash_made res = new Total_cash_made();
     }
+  
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Attendant_check.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -38,6 +40,77 @@ namespace TASK.Controllers
 
             return View();
         }
+        [HttpPost]
+        public JsonResult  insert_employees(string Industry_employees, int Supply_employees, decimal Salary)
+        {
+            //LETS CHECK IF EMPLOYEES DETAILS ARE AVAILABLE 
+            var CHECK_EMPL = _context.Employees.ToList();
+            if (CHECK_EMPL.Count!= 0)
+            {
+                //LETS JUST UPDATE
+                var TO_UPDATE = _context.Employees.FirstOrDefault();
+                TO_UPDATE.Industry_employees = Industry_employees;
+                TO_UPDATE.Supply_employees = Supply_employees;
+                TO_UPDATE.Salary = Salary;
+                _context.Entry(TO_UPDATE).State = EntityState.Modified;
+                _context.SaveChanges();
+                res.response_message = "Success, Record updatd successfully!";
+
+                //swal("Success", "Salary details updated successfully", "success");
+            }
+
+            else
+            {
+                //LETS ADD NEW RECORDS(NO RECORDS FOUND)
+                Employees x = new Employees();
+                x.Industry_employees = Industry_employees;
+                x.Supply_employees = Supply_employees;
+                x.Salary = Salary;
+                _context.Add(x);
+                _context.SaveChanges();
+                //swal("Success", "Salary details recorded successfully", "success");
+                res.response_message = "Success, Record inserted successfully!";
+            }
+            return Json(res);
+        }
+        
+        [HttpPost]
+        public JsonResult add_bottles(string transport, int quantity, string category)
+        {
+            //LETS ADD BOTTLES TO THIS CATEGORY 
+            var P_QUANTITY = _context.Cartons.FirstOrDefault(x => x.Carton_category == category);
+            int p_QNTY = P_QUANTITY.No_of_bottle;
+            int N_QNTY = p_QNTY + quantity;
+            P_QUANTITY.No_of_bottle = N_QNTY;
+            _context.Entry(P_QUANTITY).State = EntityState.Modified;
+            _context.SaveChanges();
+
+
+            ////LETS RECORD THE SALES
+            //Cartons_sold C_S = new Cartons_sold();
+            //C_S.Cartons_sold_ = quantity_five;
+            //C_S.Carton_category = category;
+            //C_S.Date = DateTime.Now.ToString("dd/MM/yyyy");
+            //C_S.Total = P_QUANTITY.Selling_price * quantity_five;
+            //_context.Add(C_S);
+            //_context.SaveChanges();
+
+            //LETS UPDATE THE DASHBOARD WITH THE NEW 
+
+            //LETS UPDATE THE EXPENSE(TRANSPORT)
+            Expenses EX = new Expenses();
+            EX.Expense = "Transport";
+            EX.Ammount = transport;
+            EX.Date = DateTime.Now.ToString("dd/MM/yyyy");
+            _context.Add(EX);
+            _context.SaveChanges();
+
+
+            //LETS UPDATE THE INTERFACE WITH THE NEW BOTTLE QUANTITY
+            res.response_message = N_QNTY.ToString();
+
+            return Json(res);
+        }
 
         public IActionResult Dashboard()
         {
@@ -45,7 +118,14 @@ namespace TASK.Controllers
             ViewBag.cartons = _context.Cartons.ToList();
             ViewBag.one_litre = _context.Cartons.FirstOrDefault(x => x.Carton_category == "1 Litre").No_of_bottle;
             ViewBag.five_litre = _context.Cartons.FirstOrDefault(x => x.Carton_category == "5 Litres").No_of_bottle;
-         
+
+            var employees = _context.Employees.FirstOrDefault();
+
+            ViewBag.industry_empl = employees.Industry_employees;
+            ViewBag.supply_empl = employees.Supply_employees;
+            ViewBag.salary = employees.Salary;
+
+          
             ViewBag.count_items = 1;
             return View();
         }public IActionResult Privacy()
